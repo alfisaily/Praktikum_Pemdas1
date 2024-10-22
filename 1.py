f7889ff046d46269b7c50ac59c4c98c91a5bded9
alfisaily <sailyalfi46@gmail.com>
2024-10-22 08:23:35 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+nilai=int(input("masukkan nilai rata rata:"))
+kehadiran = int(input("masukkan kehadiran:"))
+
+if nilai>=85 and kehadiran>=90:
+    print("Siswa layak mendapatkan beasiswa")
+else:
+    print("Siswa tidak layak mendapatkan beasiswa")
---
+++ 2.py
@@ -0,0 +1,7 @@
+penghasilan = int(input("Maasukkan penghasilan: "))
+tabungan = int(input("Masukkan tabungan"))
+
+if penghasilan>=5000000 or tabungan>=20000000:
+    print("Anda dapat membeli kendaraan")
+else:
+    print("Anda tidak dapat membeli kendaraan ")
